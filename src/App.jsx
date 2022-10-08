a30b86bb863f7dc0511c79e6a724edcb6852fa7e
serge133 <53378287+serge133@users.noreply.github.com>
2022-10-07 22:02:58 -0700

    database linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
+import "./App.css";
+import { useEffect } from "react";
+import database from "./firebase";
+import { ref, set } from "firebase/database";
 
 function App() {
+  const endpoint = "https://dinder-2-default-rtdb.firebaseio.com/";
 
-  return (
-    <div className="App">
-    </div>
-  )
+  useEffect(() => {
+    // const firebaseConfig = {
+    //   apiKey: "AIzaSyC85MEK9p9ki8uerRal7aMkHrK3p1IfzmE",
+    //   authDomain: "dinder-2.firebaseapp.com",
+    //   projectId: "dinder-2",
+    //   storageBucket: "dinder-2.appspot.com",
+    //   messagingSenderId: "637508758390",
+    //   appId: "1:637508758390:web:f0fdcc074e028a6ef06857",
+    //   databaseUrl: "https://dinder-2-default-rtdb.firebaseio.com/",
+    // };
+
+    // const app = initializeApp(firebaseConfig);
+    // const database = getDatabase(app);
+
+    set(ref(database, "test"), {
+      msg: "I Love World",
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC85MEK9p9ki8uerRal7aMkHrK3p1IfzmE",
+  authDomain: "dinder-2.firebaseapp.com",
+  projectId: "dinder-2",
+  storageBucket: "dinder-2.appspot.com",
+  messagingSenderId: "637508758390",
+  appId: "1:637508758390:web:f0fdcc074e028a6ef06857",
+  databaseUrl: "https://dinder-2-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default database;
